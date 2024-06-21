bf63544ff89e2ed36f8a4b2f29596d0f548ec5d3
shamimfstack <shamimfstack@gmail.com>
2024-06-21 20:26:30 +0600

    initial setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,8 @@
+
+export default function MainLayout() {
+    return (
+        <>
+            <h1>Main Layout</h1>
+        </>
+        );
+}
---
+++ src/Routes/router.js
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home/Home";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    }
+])
